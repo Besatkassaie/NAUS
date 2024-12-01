acd535469a63d21728cdcb8c962c369edbef2804
Besat Kassaie <bkassaie@uwaterloo.ca>
2024-12-01 14:48:46 -0500

    pre changes for si sj alignment

---
+++ enter_export.py
@@ -0,0 +1,36 @@
+import test_naive_search_export
+import logging
+
+# from scipy.stats import entropy
+# p = [0.6, 0.2, 0.2] # probability distribution
+# ent = entropy(p, base=2)
+# print("Entropy:", ent)
+
+# # Sample list of numbers
+# my_numbers = [1, '2', '3', 2, 1, 4, 2, 1]
+
+# # Count occurrences using Counter
+# from collections import Counter
+# counts = Counter(my_numbers)
+# nemvers=counts.get('2')
+# list_length=len(my_numbers)
+# prob_counts_ = {item: count / list_length for item, count in counts.items()}
+# divided_numbers = [count /list_length for count in counts.values()]
+
+# print(counts)
+
+
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+
+
+
+args2 = ['--encoder', 'cl', '--benchmark', 'santos', 
+        '--augment_op', 'drop_col','--sample_meth', 'tfidf_entity',
+        '--matching', 'exact',   '--table_order', 'column',
+        '--run_id', '0' ,   '--K','50', '--threshold', '0.7']
+
+test_naive_search_export.main(args2)
